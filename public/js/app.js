96a2afa9ceb39f09081a9618bb67c021e8d9799a
Aria Stewart <aredridel@nbtsc.org>
2015-08-12 23:10:06 -0400

    Rip out client side example, there are better ways, or this way could be reimplemented with less coupling to the server side code just as simply

--- public/js/app.js
+++ public/js/app.js
@@ -1,32 +1,5 @@
 'use strict';
 
 require(['config'], function() {
-    require(['jquery', 'nougat'], function ($, nougat) {
-        console.info('required jquery and nougat');
-        var app = {
-            initialize: function () {
-                console.info('intialized view');
-                nougat.setContext($(document.body).data());
-                //Demonstrating specialization for
-                //client side rendering.
-                this.initializeView();
-
-            },
-
-            initializeView: function () {
-                $('#more').click(function () {
-                    console.info('clicked');
-                    nougat.viewRenderer
-                        .render('nested/yinyang', {message: 'More Info'})
-                        .done(function (content) {
-                            $('#moreInfo').html(content);
-                        });
-                });
-            }
-        };
-        app.initialize();
-    });
+    // Build your app here
 });
-
-
-
